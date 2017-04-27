d7111efdc2ae6de9d645410e2ec8886db4cbf483
A_D <africandeath@gmail.com>
2017-04-27 05:35:48 +0200

    Made equality logic make sense, moved adding and removing users from Membership to Channel, added check functions to Users and Channels

--- objs.py
+++ objs.py
@@ -1,3 +1,5 @@
+import irc
+from logger import log
 
 
 class Membership:
@@ -11,40 +13,35 @@ class Membership:
         self.user = user
         self.last_ping = 0
 
-    @staticmethod
-    def addusertochan(user, chan):
-        temp = Membership(chan, user)
-        user.channels.append(temp)
-        chan.users.append(temp)
-
-    @staticmethod
-    def removeuserfromchan(user, chan):
-
-
 
 class User:
-    def __init__(self, nick, user, host):
+    def __init__(self, nick: str, user: str, host: str):
         self.nick = nick
         self.user = user
         self.host = host
         self.channels = []
         self.mask = self.nick + "!" + self.user + "@" + self.host
 
-# TODO: There must be a better way to do this, also, so, why not check if the one is in the nick
-# TODO: and if it is replace the ones in other to match? that'd work for an equality check.
-# TODO: also, nick class with this stuff there? the chan class already has some stuff for equality
-# TODO: in it, so... idk, a channel is both a name and a thing, while a nick and user /could/
-# TODO: be separate. Do I need to do this? the server will stop this before I see it, so...
-    def __eq__(self, other: str):
-        # if "{" in self.nick or "{" in other:
-        #     pass
-        # elif "}" in self.nick or "}" in other:
-        #     pass
-        # elif "|" in self.nick or "|" in other:
-        #     pass
+    def __eq__(self, other):
+        if type(other) == User:
+            other = other.nick
+        return self.nick.lower() == other.lower()
 
-        if self.nick.lower() == other.lower():
-            return True
+    @staticmethod
+    def check(mask: str, add: bool =False):
+        if mask[0] == ":":
+            mask = mask[1:]
+        log("checking users")
+        for user in irc.users:
+            if user == mask.split("!")[0]:
+                return user
+        if add:
+            n, u = mask.split("!", 1)
+            u, h = u.split("@", 1)
+            temp = User(n, u, h)
+            log("adding new user")
+            irc.users.append(temp)
+            return temp
         else:
             return False
 
@@ -52,7 +49,7 @@ class User:
 class Channel:
     def __init__(self, name: str, count: int =10, action: list =None, warn: int =7, opchan: str ="",
                  watched: bool =False):
-        self.name = name
+        self.name = name if name[0] != ":" else name[1:]
         self.modes = ""
         self.count = count
         self.action = action if action else ["MODE {chan} +b {mask}", "KICK {chan} {nick}"]
@@ -62,7 +59,51 @@ class Channel:
         self.watched = watched
 
     def __eq__(self, other: str):
-        if self.name.lower() == other.lower():
-            return True
+        return self.name.lower() == other.lower()
+
+    def adduser(self, user: User):
+        """
+        Adds a user to a channel, if the user exists in our list
+        :param user: 
+        :return: 
+        """
+        if user in irc.users:
+            temp = Membership(self, user)
+            self.users.append(temp)
+            user.channels.append(temp)
+
+    def deluser(self, user: User):
+        """
+        deletes a user from our channel if the user is in the userlist and
+        is a member of the channel
+        :param user: User object to be added to the channel
+        :return: 
+        """
+        if user in irc.users:
+            for membership in self.users:
+                if membership.user == user:
+                    self.users.remove(membership)
+                    user.channels.remove(membership)
+                    if len(user.channels) == 0:
+                        irc.users.remove(user)
+                    break
+
+    @staticmethod
+    def check(name: str, add: bool =False):
+        """
+        checks for a channel in the channel list, optionally adding it if its not found
+        :param name: name of the channel
+        :param add: whether or not to add it if its not found
+        :return: a channel object or False
+        """
+        log("checking channels")
+        for chan in irc.channels:
+            if chan == name:
+                return chan
+        if add:
+            temp = Channel(name)
+            log("adding new channel")
+            irc.channels.append(temp)
+            return temp
         else:
             return False
