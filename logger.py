a382299821941c87eb9a816fd370d96e7a04b90e
A_D <africandeath@gmail.com>
2017-04-28 23:50:28 +0200

    moved logall to logger.py

--- logger.py
+++ logger.py
@@ -1,7 +1,7 @@
 import time
 
 
-def log(data: str, ltype: str ="log",):
+def log(data: str, ltype: str = "log"):
     if ltype in ["ircin", "ircout", "log"]:
         if ltype == "ircin":
             print(time.asctime(), " : >>", data)
@@ -11,3 +11,16 @@ def log(data: str, ltype: str ="log",):
             print(time.asctime(), " : >!", data)
     else:
         raise ValueError("Unknown Log Type")
+
+
+def logall(connection):
+    log("---------------------------------------CHANNELS---------------------------------------")
+    for channel in connection.channels:
+        log(connection.channels[channel].name)
+        for membership in connection.channels[channel].users:
+            log("  `-" + connection.channels[channel].users[membership].user.mask)
+    log("----------------------------------------USERS------------------------------------------")
+    for user in connection.users:
+        log(connection.users[user].mask)
+        for channel in connection.users[user].channels:
+            log("  `-" + channel)
