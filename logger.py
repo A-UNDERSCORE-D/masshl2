df91879fff782bd327bbc99c60702e7bdd2040c0
A_D <africandeath@gmail.com>
2017-05-01 02:21:45 +0200

    Made logtypes case insensitive. fixed line length issues

--- logger.py
+++ logger.py
@@ -1,25 +1,30 @@
 import time
+import sys
 
 
 def log(data: str, ltype: str = "log"):
-    if ltype in ["ircin", "ircout", "log"]:
-        if ltype == "ircin":
+    if ltype.lower() in ["ircin", "ircout", "log", "err"]:
+        if ltype.lower() == "ircin":
             print(time.asctime(), " : >>", data)
-        elif ltype == "ircout":
+        elif ltype.lower() == "ircout":
             print(time.asctime(), " : <<", data)
-        elif ltype == "log":
+        elif ltype.lower() == "log":
             print(time.asctime(), " : >!", data)
+        elif ltype.lower() == "err":
+            print(time.asctime(), " : !!", data, file=sys.stderr)
     else:
         raise ValueError("Unknown Log Type")
 
 
 def logall(connection):
-    log("---------------------------------------CHANNELS---------------------------------------")
+    log("-----------------------------CHANNELS----------------------------")
     for channel in connection.channels:
         log(connection.channels[channel].name)
         for membership in connection.channels[channel].users:
-            log("  `-" + connection.channels[channel].users[membership].user.mask)
-    log("----------------------------------------USERS------------------------------------------")
+            log("  `-" +
+                connection.channels[channel].users[membership].user.mask)
+
+    log("-----------------------------USERS-------------------------------")
     for user in connection.users:
         log(connection.users[user].mask)
         for channel in connection.users[user].channels:
