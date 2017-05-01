003a0e2edd951722d5c61977ee9b04e0782be097
A_D <africandeath@gmail.com>
2017-05-01 02:20:59 +0200

    Rewrote cap negotiation. Fixed line length issues

--- handler.py
+++ handler.py
@@ -15,67 +15,105 @@ def handler(connection, prefix, command, args):
         onkick(connection, args)
 
     elif command == "CAP":
-        cap(connection, args)
+        handlecap(connection, args)
     elif command == "AUTHENTICATE":
-        auth(connection)
+        sendauth(connection)
 
+    elif command == "001":
+        onwelcome(connection)
     elif command == "353":
         onnames(connection, args)
-    elif command == "904":
-        connection.write("CAP END")
-    elif command == "903":
-        connection.write("CAP END")
+    elif command == "904" or command == "903":
+        capdecrement(connection)
 
 
-def auth(connection):
+def sendauth(connection):
     connection.write("AUTHENTICATE {}".format(base64.b64encode(
         (
-            connection.nick + "\00" + connection.nsuser + "\00" + connection.nspass
-         ).encode()).decode()))
+            connection.nick + "\00" + connection.nsuser + "\00" +
+            connection.nspass).encode()).decode()))
 
 
-# TODO: rewrite cap negotiation
-def cap(connection, args):
-    done = False
-    if args[1] == "LS":
-        if "userhost-in-names" in args[2]:
-            connection.write("CAP REQ :userhost-in-names")
-        if "sasl" in args[2] and connection.ssl:
-            connection.write("CAP REQ :sasl")
+def handlecap(connection, args):
+    command = args[1]
+    if command == "LS":
+        caplist = args[-1].split()
+        caps = connection.caps.intersection(caplist)
+        if len(caps) == 0:
+            connection.write("CAP END")
+
         else:
-            done = True
+            for cap in caps:
+                connection.write("CAP REQ :{}".format(cap))
+                capincrement(connection)
 
-    elif args[1] == "ACK":
-        if "userhost-in-names" in args[2]:
-            connection.uhnames = True
-        if "sasl" in args[2]:
+    elif command == "ACK":
+        cap = args[2]
+        if cap == "sasl":
+            connection.cansasl = True
+            capincrement(connection)
             connection.write("AUTHENTICATE PLAIN")
+        elif cap == "userhost-in-names":
+            connection.uhnames = True
 
-    elif args[1] == "NAK":
-        if "userhost-in-names" in args[2]:
+        capdecrement(connection)
+
+    elif command == "NAK":
+        cap = args[2]
+        capdecrement(connection)
+        if cap == "userhost-in-names":
             connection.uhnames = False
-        if "sasl" in args[2]:
-            done = True
-    if done:
+        elif cap == "sasl":
+            connection.cansasl = False
+
+
+def capincrement(connection):
+    connection.capcount += 1
+    log("capcount = " + str(connection.capcount))
+
+
+def capdecrement(connection):
+    connection.capcount -= 1
+    if connection.capcount <= 0:
         connection.write("CAP END")
+    log("capcount = " + str(connection.capcount))
+
+
+def onwelcome(connection):
+    for command in connection.commands:
+        connection.write(command)
+    if not connection.cansasl:
+        identify(connection)
+
+
+def identify(connection):
+    connection.write("PRIVMSG NickServ :IDENTIFY {nsnick} {nspass}".format(
+        nsnick=connection.nsuser, nspass=connection.nspass
+    ))
 
 
 def onnames(connection, args):
     names = args[3:]
     log(str(names))
     for mask in names:
-        if mask[-1] == " ":
-            mask = mask[:-1]
+        mask = mask.strip()
+
         # if mask[0] == "@":
 
 
 def onjoin(connection, prefix, args):
-    if not connection.channels.get(args[0], None):
-        Channel.add(connection, args[0])
-    if not connection.users.get(prefix.split("!")[0], None):
+    chan = connection.channels.get(args[0])
+    nick = prefix.split("!")[0]
+    user = connection.users.get(nick)
+    name = args[0]
+    if not chan:
+        connection.channels[name] = Channel(name)
+    if not user:
         User.add(connection, prefix)
-    if not connection.channels[args[0]].users.get(prefix.split("!")[0], None):
-        connection.channels[args[0]].adduser(connection, connection.users[prefix.split("!")[0]])
+    if not connection.channels[name].users.get(nick):
+        chan = connection.channels[name]
+        user = connection.users[nick]
+        chan.adduser(connection, user)
     logall(connection)
 
 
@@ -83,7 +121,8 @@ def onpart(connection, prefix, args):
     chan = connection.channels.get(args[0], None)
     user = connection.users.get(prefix.split("!")[0], None)
     if not chan:
-        log("WTF? I just got a part for a channel I dont have, channel was {c}".format(c=args))
+        log("WTF? I just got a part for a channel I dont have, "
+            "channel was {c}".format(c=args))
         logall(connection)
     chan.deluser(connection, user)
     logall(connection)
