6fc7471bf2f397f8ae9ad4e22234a9e15fcaa172
A_D <africandeath@gmail.com>
2017-05-06 09:55:29 +0200

    added a function to return the user's prefix in a channel

--- membership.py
+++ membership.py
@@ -1,6 +1,6 @@
 class Membership:
-    def __init__(self, channel, user, isop: bool = False, ishop: bool = False,
-                 isvoice: bool = False, isadmin: bool = False):
+    def __init__(self, channel, user, isop: bool =False, ishop: bool =False,
+                 isvoice: bool =False, isadmin: bool =False):
         self.isop = isop
         self.ishop = ishop
         self.isvoice = isvoice
@@ -8,3 +8,15 @@ class Membership:
         self.channel = channel
         self.user = user
         self.last_ping = 0
+
+    def prefix(self):
+        if self.isadmin:
+            return "!"
+        elif self.isop:
+            return "@"
+        elif self.ishop:
+            return "%"
+        elif self.isvoice:
+            return "+"
+        else:
+            return ""
