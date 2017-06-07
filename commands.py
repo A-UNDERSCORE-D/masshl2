44f3466f0804f6dfcca0d43fc7860db174a277fe
A_D <africandeath@gmail.com>
2017-06-07 19:54:23 +0200

    moved to a dict based method for calling command callbacks

--- commands.py
+++ commands.py
@@ -1,14 +1,36 @@
 from logger import *
 
+# TODO: for masshl, channel notice checking too
 
-def commands(connection, args, prefix):
-    msg = args[1]
-    if msg == "~print":
-        log("---------------MODES--------------------------")
-        log("A: " + str(connection.Amodes))
-        log("B: " + str(connection.Bmodes))
-        log("C: " + str(connection.Cmodes))
-        log("D: " + str(connection.Dmodes))
-        log("P: " + str(connection.Pmodes))
-    elif msg == "~die":
-        connection.write("QUIT :Controller requested disconnect")
+
+def printlog(connection, **kwargs):
+    logcentered("MODES")
+    log("A: " + str(connection.Amodes))
+    log("B: " + str(connection.Bmodes))
+    log("C: " + str(connection.Cmodes))
+    log("D: " + str(connection.Dmodes))
+    log("P: " + str(connection.Pmodes))
+
+
+def die(connection, **kwargs):
+    connection.quit("Controller requested disconnect")
+
+
+def nickl(connection, channel, **kwargs):
+    logcentered("nicks")
+    for nick in channel.nicklist:
+        print(nick)
+
+commands = {
+    "die": die,
+    "print": printlog,
+    "nickl": nickl
+}
+
+
+def on_command(connection, args, prefix):
+    command: str = args[1][1:]
+    if command in commands:
+        channel = connection.channels.get(args[0])
+        commands[command](connection=connection, channel=channel)
+
