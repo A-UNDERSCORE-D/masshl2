bb7342fbcdddc1cb8e8e7469082a17e42a38e732
linuxdaemon <walter@walterbarnes.net>
2017-06-20 03:20:09 -0500

    Added command registration decorator to commands.py

--- commands.py
+++ commands.py
@@ -1,9 +1,23 @@
+import inspect
+
 from logger import *
 
 # TODO: for masshl, channel notice checking too
 
+COMMANDS = {}
 
-def printlog(connection, **kwargs):
+
+def command(*cmds):
+    def _decorate(func):
+        for cmd in cmds:
+            assert cmd not in COMMANDS
+            COMMANDS[cmd] = func
+        return func
+    return _decorate
+
+
+@command("print")
+def printlog(connection):
     logcentered("MODES", connection=connection)
     log("A: " + str(connection.Amodes))
     log("B: " + str(connection.Bmodes))
@@ -12,35 +26,31 @@ def printlog(connection, **kwargs):
     log("P: " + str(connection.Pmodes))
 
 
-def die(connection, **kwargs):
+@command("die")
+def die(connection):
     connection.quit("Controller requested disconnect")
 
 
-def nickl(connection, channel, **kwargs):
+@command("nickl")
+def nickl(connection, channel):
     logcentered("nicks", connection=connection)
     for nick in channel.users.keys():
         print(nick)
 
 
-def stop(connection, **kwargs):
+@command("stop")
+def stop(connection):
     connection.bot.stop("Controller requested disconnect")
 
 
-def join(connection, cmdargs, **kwargs):
+@command("join")
+def join(connection, cmdargs):
     connection.join(cmdargs)
 
 
-def part(connection, cmdargs, **kwargs):
+@command("part")
+def part(connection, cmdargs):
     connection.part(cmdargs)
-
-commands = {
-    "die": die,
-    "print": printlog,
-    "nickl": nickl,
-    "stop": stop,
-    "join": join,
-    "part": part
-}
 
 
 def on_command(connection, args, prefix):
@@ -50,8 +60,15 @@ def on_command(connection, args, prefix):
     if temp:
         cmdargs = temp.pop(0)
 
-    if command in commands:
+    handler = COMMANDS.get(command)
+    if handler:
+        sig = inspect.signature(handler)
         channel = connection.channels.get(args[0])
-        commands[command](connection=connection,
-                          channel=channel,
-                          cmdargs=cmdargs)
+        data = {
+            "connection": connection,
+            "channel": channel,
+            "cmdargs": cmdargs,
+        }
+        args = [data[arg] for arg in sig.parameters.keys()]
+        handler(*args)
+
