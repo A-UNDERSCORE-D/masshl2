e5ff74490fb7ae15deb1733924bec8d1dca38dcb
A_D <A_D@snoonet.org>
2017-09-16 16:06:49 +0200

    edited logic

--- permissions.py
+++ permissions.py
@@ -6,21 +6,16 @@ def check(msg, perms):
     # check global admins first, then channel. then do special cases for "is_op"/hop etc
     mask = msg.origin.mask
     msg.conn.log(mask)
-    hasperm = False
     for perm in perms:
         assert perm in ("bot_control", "chan_admin")
         if perm == "bot_control":
             for c in msg.conn.admins:
                 if fnmatch(mask, c):
-                    if not hasperm:
-                        hasperm = True
-                    break
+                    return True
 
         if perm == "chan_admin":
             if msg.target is Channel:
                 for a in msg.target.admins:
                     if fnmatch(mask, a):
-                        if not hasperm:
-                            hasperm = True
-                        break
-    return hasperm
+                        return True
+    return False
