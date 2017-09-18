2b835bdc297afc207e3dca526c1e22129dcea49d
A_D <A_D@snoonet.org>
2017-09-18 14:12:56 +0200

    made permissions checks log when they succeed

--- permissions.py
+++ permissions.py
@@ -6,16 +6,19 @@ def check(msg, perms):
     # check global admins first, then channel. then do special cases for "is_op"/hop etc
     mask = msg.origin.mask
     msg.conn.log(mask)
+    allowed = False
     for perm in perms:
         assert perm in ("bot_control", "chan_admin")
         if perm == "bot_control":
             for c in msg.conn.admins:
                 if fnmatch(mask, c):
-                    return True
+                    allowed = True
 
         if perm == "chan_admin":
             if msg.target is Channel:
                 for a in msg.target.admins:
                     if fnmatch(mask, a):
-                        return True
-    return False
+                        allowed = True
+    if allowed:
+        msg.conn.log(f"Allowed {msg.origin.mask} access to {perms}")
+    return allowed
