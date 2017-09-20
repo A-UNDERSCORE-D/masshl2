1f6d58bb1151745446cb44706341490e1a8c1bbb
A_D <A_D@snoonet.org>
2017-09-20 14:24:07 +0200

    made the default that a config object uses stored on the config

--- config.py
+++ config.py
@@ -5,38 +5,38 @@ import string
 DEFAULT_CONFIG = {
     "connections": {
         "snoonet": {
-            "network": "irc.snoonet.org",
-            "port": 6697,
-            "SSL": True,
-            "user": "MHL2",
-            "nick": "MHL2",
-            "gecos": "A_D's anti mass highlight bot",
-            "nsident": "MHL",
-            "nspass": "MHLPassword",
-            "admins": ["A_D!*@*"],
-            "commands": [],
-            "cmdprefix": "~",
-            "channels": "",
-            "adminchan":
-                "#HeJustKeptTalkingInOneLongIncrediblyUnbrokenSentence",
+            "network":           "irc.snoonet.org",
+            "port":              6697,
+            "SSL":               True,
+            "user":              "MHL2",
+            "nick":              "MHL2",
+            "gecos":             "A_D's anti mass highlight bot",
+            "nsident":           "MHL",
+            "nspass":            "MHLPassword",
+            "admins":            ["A_D!*@*"],
+            "commands":          [],
+            "cmdprefix":         "~",
+            "channels":          "",
+            "adminchan":         "#HeJustKeptTalkingInOneLongIncrediblyUnbrokenSentence",
             "global_nickignore": [l for l in string.ascii_lowercase],
-            "bot_nicks": [],
+            "bot_nicks":         [],
             "global_maskignore": "",
             # debug or similar configs
-            "print_raw": True
+            "print_raw":         True
         },
     },
 
-    "debug": True,
+    "debug":       True,
 }
 
 
 class Config(dict):
-    def __init__(self, name: str = "config"):
+    def __init__(self, name: str = "config", default: dict = DEFAULT_CONFIG):
         super().__init__()
         self.name = name
+        self.__default = default
         self.clear()
-        self.update(DEFAULT_CONFIG)
+        self.update(self.__default)
         self.load(self.name)
 
     def load(self, name=None):
@@ -44,7 +44,7 @@ class Config(dict):
             name = self.name
         if not os.path.exists(name + ".json"):
             with open(name + ".json", "w") as f:
-                json.dump(DEFAULT_CONFIG, f, indent=2)
+                json.dump(self.__default, f, indent=2)
 
         with open(name + ".json") as f:
             self.update(json.load(f))
@@ -56,7 +56,7 @@ class Config(dict):
             json.dump(self, f, indent=2)
 
     def update_f_m(self):
-        self.update(DEFAULT_CONFIG)
+        self.update(self.__default)
         with open(self.name + ".json") as f:
             self.update(json.load(f))
         with open(self.name + ".json", "w") as f:
