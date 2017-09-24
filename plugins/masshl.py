22b92048047b134960df29b2a66653a10f592672
A_D <A_D@snoonet.org>
2017-09-24 16:57:56 +0200

    added load hook for masshl, loads data onto channels

--- plugins/masshl.py
+++ plugins/masshl.py
@@ -1,34 +1,22 @@
-from handler import hook_message
-from parser import Message
+from hook import message, command, load, unload
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from channel import Channel
+    from user import User
+    from connection import Connection
 
 
-def sanitise_nicklist(msg: 'Message') -> set:
-    nicks = set()
-    for nick in msg.target.memberships:
-        if len(nick) < 2 or nick == msg.origin or nick in msg.conn.global_nickignore or nick in msg.target.nick_ignore:
-            continue
-        nicks.add(nick)
-    return nicks
+@load
+def onload(bot):
+    bot.log("MASSHL PLUGIN: running on load hooks")
+    if "masshl" not in bot.config:
+        return
+    config = bot.config["masshl"]
+    bot.log(config)
+    for conn in bot.connections:
+        for channel in conn.channels:
+            _load_data_onto_channel(channel, config[conn.name][channel.name])
 
 
-def check_msg(msg: 'Message') -> int:
-    nick_list = sanitise_nicklist(msg)
-    checked_nicks = set()
-    count = 0
-
-    for nick in nick_list:
-        if nick in msg and nick not in checked_nicks:
-            checked_nicks.add(nick)
-            count += 1
-            if nick in msg.conn.bot_nicks:
-                count += 1
-    return count
-
-
-@hook_message
-def on_msg(msg: 'Message'):
-    if msg.is_chan_message:
-        count = check_msg(msg)
-        msg.conn.log(f"Count: {count}")
-        if count > 0:
-            return f"count was: {count}"
+def _load_data_onto_channel(channel: 'Channel', config: dict):
+    channel.storage["masshl"].update(config)
