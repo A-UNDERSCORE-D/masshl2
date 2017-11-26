58c5bb309f4fd4644d62b575182f523e31df2782
A_D <A_D@snoonet.org>
2017-11-26 15:38:12 +0200

    added extra debug commands for event managers

--- plugins/debug_commands.py
+++ plugins/debug_commands.py
@@ -1,15 +1,37 @@
+from typing import TYPE_CHECKING
 from hook import command
 import json
 
+if TYPE_CHECKING:
+    from bot import Bot
+    from event import EventManager
+
 
 @command("event_manager", "bot_control")
-def eventmanager_command(bot, event_manager, args):
+def eventmanager_command(bot: 'Bot', event_manager: 'EventManager', args):
     if len(args) < 1:
         return "You must supply an argument."
     subcommand = args.pop(0).lower()
 
-    if subcommand == "debug":
+    if subcommand == "toggle_debug":
         event_manager.debug = not event_manager.debug
+
+    elif subcommand == "print":
+        print(event_manager.events)
+        return "Dumped to stdout."
+
+    elif subcommand == "pretty_print":
+        for event in event_manager.events:
+            print(event)
+            for hook in event_manager.events[event]:
+                print(f"`- {hook}")
+        return "Dumped to stdout."
+
+    elif subcommand == "cleanup":
+        def cleanup():
+            event_manager._cleanup_events()
+        return cleanup
+
 
 
 @command("command_debug")
@@ -19,3 +41,8 @@ def command_debug(bot):
         tmp[e] = [str(x) for x in bot.event_manager.events[e]]
     print(json.dumps(tmp, indent=2))
     return "Dumped to stdout"
+
+
+@command("dump_connections")
+def conn_debug(bot):
+    return bot.connections
